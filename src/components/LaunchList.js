4c1cd0f364052860c4b7ad65a90945506c87d437
marisol <mtoscanor37@gmail.com>
2019-06-25 21:15:24 +0100

    Fixed bug when selecting 2011 from the dropdown in render data

--- src/components/LaunchList.js
+++ src/components/LaunchList.js
@@ -4,17 +4,6 @@ import './LaunchList.css';
 
 class LaunchList extends Component {
   render() {
-    const launchesNodes = this.props.spaceLaunches.map((launch) => {
-      return (
-        <Launch
-        key={ launch.flight_number }
-        flightNumber={ launch.flight_number }
-        missionName={ launch.mission_name }
-        launchDate={ launch.launch_date_utc }
-        rocketName={ launch.rocket.rocket_name }
-        />
-      );
-    })
 
     const newSelectedLaunches = this.props.selectedSpaceLaunches.map((launch) => {
       return (
@@ -31,7 +20,7 @@ class LaunchList extends Component {
 
     return (
       <div className="launch-list">
-        {newSelectedLaunches.length > 0 ? newSelectedLaunches : launchesNodes}
+        {newSelectedLaunches}
       </div>
     );
   }
